7526cd667ad5970d9b2ad104a397dd66e3976369
Favourdoreen <kelmadoreen@gmail.com>
2023-02-26 04:23:29 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - First Entry
+ *
+<<<<<<< HEAD
+ * return: Always 1 (Success)
+=======
+ * Return: Always 1 (Success)
+>>>>>>> de313c14a807d6cd65872ea57fea695f57992ac7
+ */
+int main(void)
+{
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
