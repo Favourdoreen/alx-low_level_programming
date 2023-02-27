6b8f660cfbdcad07a80db6825036396a7a8eff66
Favourdoreen <kelmadoreen@gmail.com>
2023-02-27 09:15:24 -0800

    6-puts2.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -6,7 +6,7 @@
 *
 *@n: An integer input pointer
 *
-*Return : nothing
+*Return
 */
 void reset_to_98(int *n)
 {
--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,4 +1,4 @@
-include "main.h"
+#include "main.h"
 
 /**
  * puts2 - prints every other character of string
@@ -6,7 +6,6 @@ include "main.h"
  * Return: void
  */
 void puts2(char *str)
-
 {
 
 int longi = 0;
