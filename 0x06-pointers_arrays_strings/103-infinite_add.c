d4aaae838454e06c7df1dcfd13d20f91f3e0cf56
Favourdoreen <kelmadoreen@gmail.com>
2023-03-02 17:07:01 -0800

    101-print_number.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -19,40 +19,30 @@ char *add_strings(char *n1, char *n2, char *r, int r_index)
 int num, tens = 0;
 for (; *n1 && *n2; n1--, r_index--)
 {
-	num = (*n1 - '0') + (*n2 - '0');
-	num += tens;
-	*(r + r_index) =(num % 10) + '0';
-	tens = num / 10;
+num = (*n1 - '0') + (*n2 - '0');
+num += tens;
+*(r + r_index) = (num % 10) + '0';
+tens = num / 10;
 }
-for (; *n1
+for (; *n1; n1--, r_index--)
 {
-i++;
+num = (*n1 - '0') + tens;
+*(r + r_index) = (num % 10) + '0';
+tens = num / 10;
 }
-i--;
-
-for (j = 0; j < i; j++, i--)
+for (; *n2; n2--, r_index--)
 {
-temp = *(n + j);
-*(n + j) = *(n + i);
-*(n + i) = temp;
+num = (*n2 - '0') + tens;
+*(r + r_index) = (num % 10) + '0';
+tens = num / 10;
 }
-}
-
-/**
- * infinite_add - add 2 numbers together
- * @n1: text representation of 1st number to add
- * @n2: text representation of 2nd number to add
- * @r: pointer to buffer
- * @size_r: buffer size
- * Return: pointer to calling function
- */
-
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
+if (tens && r_index >= 0)
 {
-int overflow = 0, i = 0, j = 0, digits = 0;
-int val1 = 0, val2 = 0, temp_tot = 0;
+*(r + r_index) = (tens % 10) + '0';
+return (r + r_index);
+}
+else if (tens && r_index < 0)
+return (0);
 
-while (*(n1 + i) != '\0')
-i++;
-while (*(n2 + j) != '\0')
+return (r + r_index + 1);
 }
