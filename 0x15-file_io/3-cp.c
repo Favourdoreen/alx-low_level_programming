26644d4edad555e739e178eee76614e22714a22c
Favourdoreen <kelmadoreen@gmail.com>
2023-04-08 06:42:04 -0700

    3-cp.c

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,71 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * error_file - checks if files can be opened.
+ * @file_from: file_from.
+ * @file_to: file_to.
+ * @argv: arguments vector.
+ * Return: no return.
+ */
+void error_file(int file_from, int file_to, char *argv[])
+{
+if (file_from == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+exit(98);
+}
+if (file_to == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+exit(99);
+}
+}
+/**
+ * main - Copies the contents of a file to another file.
+ * @argc: number of arguments
+ * @argv: array of pointers to the arguments.
+ * Return: Always 0.
+ * If the argument count is incorrect - exit code 97.
+ * If file_from does not exist or cannot be read - exit code 98
+ * If file_to cannot be created or written to - exit code 99.
+ *  If file_to or file_from cannot be closed - exit code 100
+ */
+int main(int argc, char *argv[])
+{
+int from, to, err_close;
+ssize_t nchars, nwr;
+char buf[1024];
+
+if (argc != 3)
+{
+dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
+exit(97);
+}
+from = open(argv[1], O_RDONLY);
+to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+error_file(from, to, argv);
+nchars = 1024;
+while (nchars == 1024)
+{
+nchars = read(from, buf, 1024);
+if (nchars == -1)
+error_file(-1, 0, argv);
+nwr = write(to, buf, nchars);
+if (nwr == -1)
+error_file(0, -1, argv);
+}
+err_close = close(from);
+if (err_close == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+exit(100);
+}
+err_close = close(to);
+if (err_close == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+exit(100);
+}
+return (0);
+}
