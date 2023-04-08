421971382a552ea9516efa7b533c4737fdf247b2
Favourdoreen <kelmadoreen@gmail.com>
2023-04-08 06:11:09 -0700

    2-append_text_to_file.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,25 +9,25 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-int fr;
+int fd;
 ssize_t n, m;
 char *buf;
 
 if (!filename)
 return (0);
 
-fr = open(filename, O_RDONLY);
+fd = open(filename, O_RDONLY);
 
-if (fr == -1)
+if (fd == -1)
 return (0);
 
 buf = malloc(sizeof(char) * (letters));
 if (!buf)
 return (0);
 
-n = read(fr, buf, letters);
+n = read(fd, buf, letters);
 m = write(STDOUT_FILENO, buf, nrd);
-close(fr);
+close(fd);
 free(buf);
 
 return (m);
