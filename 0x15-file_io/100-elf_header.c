dd876f74513a566fa658a43f4636c20b3fb059ca
Favourdoreen <kelmadoreen@gmail.com>
2023-09-04 14:00:49 -0700

    bn

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -18,21 +18,20 @@ void print_entry(unsigned long int e_entry, unsigned char *e_ident);
 void close_elf(int elf);
 
 /**
- * check_elf - Checks if a file is an ELF file.
- * @e_ident: A pointer to an array containing the ELF magic numbers.
- *
+ * check_elf - Checks if file is an ELF file.
+ * @e_ident: pointer to an array containing the ELF magic numbers.
  * Description: If the file is not an ELF file - exit code 98.
  */
 void check_elf(unsigned char *e_ident)
 {
-int index;
+int a;
 
-for (index = 0; index < 4; index++)
+for (a = 0; a < 4; a++)
 {
-if (e_ident[index] != 127 &&
-e_ident[index] != 'E' &&
-e_ident[index] != 'L' &&
-e_ident[index] != 'F')
+if (e_ident[a] != 127 &&
+e_ident[a] != 'E' &&
+e_ident[a] != 'L' &&
+e_ident[a] != 'F')
 {
 dprintf(STDERR_FILENO, "Error: Not an ELF file\n");
 exit(98);
@@ -41,22 +40,21 @@ exit(98);
 }
 
 /**
- * print_magic - Prints the magic numbers of an ELF header.
- * @e_ident: A pointer to an array containing the ELF magic numbers.
- *
+ * print_magic - Prints the magic numbers of ELF
+ * @e_ident: A pointer to an array containing  magic numbers.
  * Description: Magic numbers are separated by spaces.
  */
 void print_magic(unsigned char *e_ident)
 {
-int index;
+int a;
 
 printf(" Magic: ");
 
-for (index = 0; index < EI_NIDENT; index++)
+for (a = 0; a < EI_NIDENT; a++)
 {
-printf("%02x", e_ident[index]);
+printf("%02x", e_ident[a]);
 
-if (index == EI_NIDENT - 1)
+if (a == EI_NIDENT - 1)
 printf("\n");
 else
 printf(" ");
@@ -64,8 +62,8 @@ printf(" ");
 }
 
 /**
- * print_class - Prints the class of an ELF header.
- * @e_ident: A pointer to an array containing the ELF class.
+ * print_class - Prints class of an ELF
+ * @e_ident: A pointer to an array containing the ELF
  */
 void print_class(unsigned char *e_ident)
 {
@@ -88,8 +86,8 @@ printf("<unknown: %x>\n", e_ident[EI_CLASS]);
 }
 
 /**
- * print_data - Prints the data of an ELF header.
- * @e_ident: A pointer to an array containing the ELF class.
+ * print_data - Prints the data of an ELF
+ * @e_ident: A pointer to an array containing the ELF
  */
 void print_data(unsigned char *e_ident)
 {
@@ -112,8 +110,8 @@ printf("<unknown: %x>\n", e_ident[EI_CLASS]);
 }
 
 /**
- *  * print_version - Prints the version of an ELF header.
- *   * @e_ident: A pointer to an array containing the ELF version.
+ *  * print_version - Prints the version of an ELF
+ *   * @e_ident: A pointer to an array containing the ELF
  *    */
 void print_version(unsigned char *e_ident)
 {
@@ -132,8 +130,8 @@ break;
 }
 
 /**
- * print_osabi - Prints the OS/ABI of an ELF header.
- * @e_ident: A pointer to an array containing the ELF version.
+ * print_osabi - Prints the OS of an ELF 
+ * @e_ident: A pointer to an array containing the ELF
  */
 void print_osabi(unsigned char *e_ident)
 {
@@ -177,8 +175,8 @@ printf("<unknown: %x>\n", e_ident[EI_OSABI]);
 }
 
 /**
- * print_abi - Prints the ABI version of an ELF header.
- * @e_ident: A pointer to an array containing the ELF ABI version.
+ * print_abi - Prints the ABI version of an ELF
+ * @e_ident: A pointer to an array containing the ELF
  */
 void print_abi(unsigned char *e_ident)
 {
@@ -187,9 +185,9 @@ e_ident[EI_ABIVERSION]);
 }
 
 /**
- * print_type - Prints the type of an ELF header.
+ * print_type - Prints type of an ELF header.
  * @e_type: The ELF type.
- * @e_ident: A pointer to an array containing the ELF class.
+ * @e_ident: A pointer to  array containing the ELF class.
  */
 void print_type(unsigned int e_type, unsigned char *e_ident)
 {
@@ -221,62 +219,57 @@ printf("<unknown: %x>\n", e_type);
 }
 
 /**
- * print_entry - Prints the entry point of an ELF header.
- * @e_entry: The address of the ELF entry point.
- * @e_ident: A pointer to an array containing the ELF class.
+ * print_entry - Prints the entry point of an ELF
+ * @e_entry: address of the ELF entry point.
+ * @e_ident:  pointer to an array containing the ELF 
  */
 void print_entry(unsigned long int e_entry, unsigned char *e_ident)
 {
-	printf(" Entry point address: ");
+printf(" Entry point address: ");
 
-	if (e_ident[EI_DATA] == ELFDATA2MSB)
-	{
-		e_entry = ((e_entry << 8) & 0xFF00FF00) |
-			  ((e_entry >> 8) & 0xFF00FF);
-		e_entry = (e_entry << 16) | (e_entry >> 16);
-	}
+if (e_ident[EI_DATA] == ELFDATA2MSB)
+{
+e_entry = ((e_entry << 8) & 0xFF00FF00) |
+((e_entry >> 8) & 0xFF00FF);
+e_entry = (e_entry << 16) | (e_entry >> 16);
+}
 
-	if (e_ident[EI_CLASS] == ELFCLASS32)
-		printf("%#x\n", (unsigned int)e_entry);
+if (e_ident[EI_CLASS] == ELFCLASS32)
+printf("%#x\n", (unsigned int)e_entry);
 
-	else
-		printf("%#lx\n", e_entry);
+else
+printf("%#lx\n", e_entry);
 }
 
 /**
- * close_elf - Closes an ELF file.
- * @elf: The file descriptor of the ELF file.
- *
- * Description: If the file cannot be closed - exit code 98.
+ * close_elf - Closes an ELF document
+ * @elf: The file descriptor of the ELF document
+ * Description: If file cannot be closed - exit code 98.
  */
 void close_elf(int elf)
 {
 if (close(elf) == -1)
 {
 dprintf(STDERR_FILENO,
-"Error: Can't close fd %d\n", elf);
+"Error: Can't close cf %d\n", elf);
 exit(98);
 }
 }
 
 /**
- * main - Displays the information contained in the
- * ELF header at the start of an ELF file.
- * @argc: The number of arguments supplied to the program.
+ * main - Displays the info contained in the ELF
+ * @argc: number of arguments supplied to the program.
  * @argv: An array of pointers to the arguments.
- *
  * Return: 0 on success.
  *
- * Description: If the file is not an ELF File or
- * the function fails - exit code 98.
  */
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 Elf64_Ehdr *header;
-int o, r;
+int p, s;
 
-o = open(argv[1], O_RDONLY);
-if (o == -1)
+p = open(argv[1], O_RDONLY);
+if (p == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
 exit(98);
@@ -288,11 +281,11 @@ close_elf(o);
 dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
 exit(98);
 }
-r = read(o, header, sizeof(Elf64_Ehdr));
-if (r == -1)
+s = read(p, header, sizeof(Elf64_Ehdr));
+if (s == -1)
 {
 free(header);
-close_elf(o);
+close_elf(p);
 dprintf(STDERR_FILENO, "Error: `%s`: No such file\n", argv[1]);
 exit(98);
 }
@@ -309,6 +302,6 @@ print_type(header->e_type, header->e_ident);
 print_entry(header->e_entry, header->e_ident);
 
 free(header);
-close_elf(o);
+close_elf(p);
 return (0);
 }
--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,71 +1,100 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
+
+char *create_buffer(char *file);
+void close_file(int cf);
 
 /**
- * error_file - checks if files can be opened.
- * @from: from.
- * @to: to.
- * @argv: arguments vector.
- * Return: no return.
+ * create_buffer - Allocates 1024 bytes for  buffer.
+ * @file: The name of the file is storing chars for.
+ * Return: A pointer to the new allocated buffer.
  */
-void error_file(int from, int to, char *argv[])
+char *create_buffer(char *file)
 {
-if (from == -1)
+char *buffer;
+
+buffer = malloc(sizeof(char) * 1024);
+
+if (buffer == NULL)
 {
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-exit(98);
-}
-if (to == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+dprintf(STDERR_FILENO,
+"Error: Can't write to %s\n", file);
 exit(99);
 }
+
+return (buffer);
 }
+
 /**
- * main - Copies the contents of a file to another file.
- * @argc: number of arguments
- * @argv: array of pointers to the arguments.
- * Return: Always 0.
- * If the argument count is incorrect - exit code 97.
- * If from does not exist or cannot be read - exit code 98
- * If to cannot be created or written to - exit code 99.
- *  If to or from cannot be closed - exit code 100
+ * close_file - Closes file
+ * @cf: The file descriptor to be closed.
+ */
+void close_file(int cf)
+{
+int z;
+
+z = close(cf);
+
+if (z == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't close cf %d\n", cf);
+exit(100);
+}
+}
+
+/**
+ * main - Copies the contents of a file.
+ * @argc: number of arguments supplied to the program.
+ * @argv: An array of pointers
+ * Return: 0 on success.
+ * Description: If argument count is incorrect - exit code 97.
+ * If file_from does not exist - exit code 98.
+ * If file_to cannot be  written to - exit code 99.
+ * If file_to or file_from cannot be closed - exit code 100.
  */
 int main(int argc, char *argv[])
 {
-int from, to, err_close;
-ssize_t nchars, nwr;
-char buf[1024];
+int fr, tt, rr, ww;
+char *buffer;
 
 if (argc != 3)
 {
-dprintf(STDERR_FILENO, "%s\n", "Usage: cp from to");
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 exit(97);
 }
-from = open(argv[1], O_RDONLY);
-to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-error_file(from, to, argv);
-nchars = 1024;
-while (nchars == 1024)
+
+buffer = create_buffer(argv[2]);
+fr = open(argv[1], O_RDONLY);
+rr = read(fr, buffer, 1024);
+tt = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+do {
+if (fr == -1 || rr == -1)
 {
-nchars = read(from, buf, 1024);
-if (nchars == -1)
-error_file(-1, 0, argv);
-nwr = write(to, buf, nchars);
-if (nwr == -1)
-error_file(0, -1, argv);
+dprintf(STDERR_FILENO,
+"Error: Can't read from file %s\n", argv[1]);
+free(buffer);
+exit(98);
 }
-err_close = close(from);
-if (err_close == -1)
+
+ww = write(tt, buffer, rr);
+if (tt == -1 || ww == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from);
-exit(100);
+dprintf(STDERR_FILENO,
+"Error: Can't write to %s\n", argv[2]);
+free(buffer);
+exit(99);
 }
-err_close = close(to);
-if (err_close == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from);
-exit(100);
-}
+
+rr = read(fr, buffer, 1024);
+tt = open(argv[2], O_WRONLY | O_APPEND);
+
+} while (rr > 0);
+
+free(buffer);
+close_file(fr);
+close_file(tt);
+
 return (0);
 }
